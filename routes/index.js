b6af28e44ff87b5135afad684f2c54eb79bda4c4
Kerem Dinçer <keremdincer@npi.local>
2018-07-18 17:10:30 +0300

    Yeni rapor ve login sayfaları oluşturuldu. Demo data oluşturuldu.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
-var express = require('express')
-var router = express.Router()
+const express = require('express')
+const router = express.Router()
+
+const dataContainer = require('./../databases/data-container')
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Güvenlik Raporlama Sistemi' })
+  res.render('dashboard', { title: 'Güvenlik Raporlama Sistemi - Kontrol Paneli' })
+})
+
+router.get('/new', (req, res, next) => {
+  res.render(
+    'newReport',
+    {
+      title: 'Güvenlik Raporlama Sistemi - Yeni Rapor',
+      data: dataContainer
+    }
+  )
+})
+
+router.get('/reports', (req, res, next) => {
+  res.render('reports')
+})
+
+router.get('/logout', (req, res, next) => {
+  res.render('login')
+})
+
+router.post('/login', (req, res, next) => {
+  res.render('/dashboard')
 })
 
 module.exports = router
