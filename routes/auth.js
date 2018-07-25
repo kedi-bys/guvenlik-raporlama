23c94ea6de0a859442fd8c25b0592d832c6ec18a
Sefa Koç <koc.sefa@hotmail.com>
2018-07-25 17:45:10 +0300

    oturum açan kullanıcının veri tabanı kaydı oluşturulup kontrolü sağlandı

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const ActiveDirectory = require('activedirectory')
 const activeDirectoryConfig = require('./../config').activeDirectoryConfig
+const users = require('./../models/users.model')
 
 // GET Login
 router.get('/login', (req, res, next) => {
@@ -24,8 +25,6 @@ router.post('/login', (req, res, next) => {
   const username = req.body.username
   const password = req.body.password
 
-  console.log(username, password)
-
   let adConfig = activeDirectoryConfig
   const ad = new ActiveDirectory(adConfig)
 
@@ -36,11 +35,12 @@ router.post('/login', (req, res, next) => {
       adConfig.password = password
 
       new ActiveDirectory(adConfig)
-        .findUser(username, (err, user) => {
+        .findUser(username, async (err, user) => {
           if (err) console.log(err)
           if (!user) {
             res.send({ status: 'Kullanıcı bilgileri bulunamadı.' })
           } else {
+            // kullanıcı AD içinde bulunursa session oluşturur
             req.session.user = {
               username: user.sAMAccountName,
               mail: user.mail,
@@ -48,7 +48,43 @@ router.post('/login', (req, res, next) => {
               name: user.givenName,
               surname: user.sn
             }
-            console.log(req.body.next)
+            // kullanıcı veri tabanından kontrol edilir
+            let _user = null
+            try {
+              _user = await users.findOne({ 'username': user.sAMAccountName })
+              // kullanıcı veri tabanında bulunmuyorsa yenisini oluşturur
+              if (_user === null) {
+                let group = user.dn.split(',')
+                  .filter(g => g.indexOf('OU') !== -1)
+                group = group[0] !== undefined ? group[0].replace('OU=', '') : null
+                let data = {
+                  displayname: user.displayName,
+                  username: user.sAMAccountName,
+                  mail: user.mail,
+                  group: group,
+                  role: {
+                    name: 'Kullanıcı',
+                    privilege: 1
+                  },
+                  firstlogin: Date(),
+                  lastlogin: Date(),
+                  prevlogin: null
+                }
+                users.insertMany(data).catch((err) => {
+                  console.log(err)
+                })
+              } else {
+                // kulanıcı varsa son giriş tarihi düzenlenir
+                users.update({ _id: _user._id }, {$set: {prevlogin: _user.lastlogin, lastlogin: Date()}}, (err, success) => {
+                  if (err) {
+                    console.error(err)
+                  }
+                })
+              }
+            } catch (error) {
+              console.log(error)
+            }
+
             res.redirect(req.body.next || '/')
           }
         })
