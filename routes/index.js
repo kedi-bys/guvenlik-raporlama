32159741e517696ad49e9bda4a827dc7f1fa0f60
Kerem Dinçer <keremdincer@npi.local>
2018-08-03 18:31:07 +0300

    Gereksiz kod parçaları temizlendi.

--- routes/index.js
+++ routes/index.js
@@ -5,27 +5,27 @@ const report = require('./../models/report.model')
 router.get('/', async (req, res, next) => {
   if (!req.session.user) {
     res.redirect('/auth/login?next=' + req.originalUrl)
-  } else {
-    const username = req.session.user.username
-
-    const userReports = await report.find({ creatorId: username })
-    const createdReports = await report.find({ status: { $in: ['Oluşturuldu', 'Paraflandı'] }, creatorId: username }).count()
-    const rejectedReports = await report.find({ creatorId: username, status: 'İptal edildi' }).count()
-    const approvedReports = await report.find({ creatorId: username, status: 'Onaylandı' }).count()
-
-    res.render(
-      'dashboard',
-      {
-        title: 'Güvenlik Raporlama Sistemi - Kontrol Paneli',
-        page: 'ControlPanel',
-        user: req.session.user,
-        userReports,
-        createdReports,
-        rejectedReports,
-        approvedReports
-      }
-    )
+    return
   }
+  const username = req.session.user.username
+
+  const userReports = await report.find({ creatorId: username })
+  const createdReports = await report.find({ status: { $in: ['Oluşturuldu', 'Paraflandı'] }, creatorId: username }).count()
+  const rejectedReports = await report.find({ creatorId: username, status: 'İptal edildi' }).count()
+  const approvedReports = await report.find({ creatorId: username, status: 'Onaylandı' }).count()
+
+  res.render(
+    'dashboard',
+    {
+      title: 'Güvenlik Raporlama Sistemi - Kontrol Paneli',
+      page: 'ControlPanel',
+      user: req.session.user,
+      userReports,
+      createdReports,
+      rejectedReports,
+      approvedReports
+    }
+  )
 })
 
 module.exports = router
